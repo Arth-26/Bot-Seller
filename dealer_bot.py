ef79cd2f05f9b67caa1fa7b68ea7c6d0607c3c06
Arth-23 <arthuralves@ads.fiponline.edu.br>
2024-11-08 16:59:14 -0300

    Todas as funções de mostrar descontos de jogos funcionando

--- dealer_bot.py
+++ dealer_bot.py
@@ -14,6 +14,9 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+
+""" DEALS FUNCTIONS """
+
 @bot.event
 async def on_ready():
     print('Estou funcionando! Pode começar a usar nossos comandos!')
@@ -60,5 +63,76 @@ async def best_deals(message):
         count += 1
     await message.channel.send(game_list)
 
+@bot.command(name='freeGames')
+async def best_deals(message):
+    await message.channel.send('Espere um pouco, estamos processando!')
+
+    scraping_bot = ScrapingBot()
+    games = scraping_bot.see_free_games()
+    count = 0
+    game_list = ''
+
+    for game, info in games.items():
+        if count == 10:
+            await message.channel.send(game_list)
+            await message.channel.send('-------------------------------------------------------------')
+            game_list = ''
+            count = 0
+        for price, discount in info.items():
+            display_message = f'{game}: {price} | {discount}'
+        game_list += f'{display_message}\n'
+        count += 1
+    await message.channel.send(game_list)
+
+@bot.command(name='newDeals')
+async def best_deals(message):
+    await message.channel.send('Espere um pouco, estamos processando!')
+
+    scraping_bot = ScrapingBot()
+    games = scraping_bot.see_new_deals()
+    count = 0
+    game_list = ''
+
+    for game, info in games.items():
+        if count == 10:
+            await message.channel.send(game_list)
+            await message.channel.send('-------------------------------------------------------------')
+            game_list = ''
+            count = 0
+        for price, discount in info.items():
+            display_message = f'{game}: {price} | {discount}'
+        game_list += f'{display_message}\n'
+        count += 1
+    await message.channel.send(game_list)
+
+@bot.command(name='historicalLow')
+async def best_deals(message):
+    await message.channel.send('Espere um pouco, estamos processando!')
+
+    scraping_bot = ScrapingBot()
+    games = scraping_bot.see_game_historical_low()
+    count = 0
+    game_list = ''
+
+    for game, info in games.items():
+        if count == 10:
+            await message.channel.send(game_list)
+            await message.channel.send('-------------------------------------------------------------')
+            game_list = ''
+            count = 0
+        for price, discount in info.items():
+            display_message = f'{game}: {price} | {discount}'
+        game_list += f'{display_message}\n'
+        count += 1
+    await message.channel.send(game_list)
+
     
+
+""" NEWS FUNCTIONS """
+
+@bot.command(name='dailyNews')
+async def daily_news(message):
+    pass
+
+
 bot.run(token)
